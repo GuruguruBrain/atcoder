20c95bc0b4814e1eede7a67fa208afefb941c03c
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-06-24 00:47:17 +0900

    solved ABC085C

---
+++ src/abselection/ABC049C/Main.java
@@ -0,0 +1,11 @@
+// 白昼夢
+
+package abselection.ABC049C;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/abselection/ABC085C/Main.java
+++ src/abselection/ABC085C/Main.java
@@ -1,5 +1,6 @@
 // Otoshidama
 
+// My answer
 package abselection.ABC085C;
 
 import java.util.Scanner;
@@ -27,3 +28,35 @@ public class Main {
         System.out.println(qty[0] + " " + qty[1] + " " + qty[2]);
     }
 }
+
+
+// Model answer
+/*
+
+
+package abselection.ABC085C;
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int Y = sc.nextInt();
+
+        for (int a=0; a<=N; a++) {
+            for (int b=0; b<=N-a; b++) {
+                int c = N - a - b;
+                int total = a * 10000 + b * 5000 + c * 1000;
+                if (total == Y) {
+                    System.out.println(a + " " + b + " " + c);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("-1 -1 -1");
+    }
+}
+
+*/
