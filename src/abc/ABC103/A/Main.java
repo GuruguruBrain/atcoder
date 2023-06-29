00b460498e83808706abdde8539403b60680924a
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-06-29 17:47:29 +0900

    solved ABC102A

---
+++ src/abc/ABC103/A/Main.java
@@ -0,0 +1,9 @@
+// Task Scheduling Problem
+
+package abc.ABC103.A;
+
+class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/abc/ABC103/B/Main.java
@@ -0,0 +1,9 @@
+// String Rotation
+
+package abc.ABC103.B;
+
+class Main {
+	public static void main(String[] args) {
+		
+	}
+}
