15fde9dd086db00328111eb7e80d49a32bdc79f0
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-07-02 18:32:16 +0900

    add ABC105A and ABC105B

---
+++ src/abc/ABC105/A/Main.java
@@ -0,0 +1,9 @@
+// AtCoder Crackers
+
+package abc.ABC105.A;
+
+class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/abc/ABC105/B/Main.java
@@ -0,0 +1,9 @@
+// Cakes and Donuts
+
+package abc.ABC105.B;
+
+class Main {
+	public static void main(String[] args) {
+		
+	}
+}
