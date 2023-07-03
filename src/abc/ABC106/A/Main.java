6312542d4fd8c62673adfaa1baf5a0a9a5add517
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-07-03 23:22:05 +0900

    add ABC106AB

---
+++ src/abc/ABC106/A/Main.java
@@ -0,0 +1,8 @@
+// Garden
+
+package abc.ABC106.A;
+
+class Main {
+	public static void main(String[] args) {
+	}
+}
---
+++ src/abc/ABC106/B/Main.java
@@ -0,0 +1,9 @@
+// 105
+
+package abc.ABC106.B;
+
+class Main {
+	public static void main(String[] args) {
+		
+	}
+}
