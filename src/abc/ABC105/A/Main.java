9474a93bde1157c865f3ca929286ebb6583e02ab
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-07-03 23:16:21 +0900

    solved ABC105B

--- src/abc/ABC105/A/Main.java
+++ src/abc/ABC105/A/Main.java
@@ -2,8 +2,18 @@
 
 package abc.ABC105.A;
 
+import java.util.Scanner;
+
 class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
 		
+		if (N % K == 0) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
 	}
 }
