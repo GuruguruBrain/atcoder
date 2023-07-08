04b9e8145e6a25452cd9af734143da5300e448f5
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-07-08 23:54:20 +0900

    participated ABC309AB

--- src/abc/ABC106/B/Main.java
+++ src/abc/ABC106/B/Main.java
@@ -2,8 +2,26 @@
 
 package abc.ABC106.B;
 
+import java.util.Scanner;
+
 class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int sum = 0;
+		int total = 0;
 		
+		for (int i = 0; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (N % j == 0) {
+					sum++;
+					if (sum == 8) {
+						total++;
+					}
+				}
+			}
+			
+		}
+		System.out.println(total);			
 	}
 }
