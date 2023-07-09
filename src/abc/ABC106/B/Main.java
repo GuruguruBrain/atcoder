31a57fba10b051b7aa57a515cd203119657fb705
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-07-09 15:41:48 +0900

    solved ABC106B

--- src/abc/ABC106/B/Main.java
+++ src/abc/ABC106/B/Main.java
@@ -10,17 +10,17 @@ class Main {
 		int N = sc.nextInt();
 		int sum = 0;
 		int total = 0;
-		
-		for (int i = 0; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (N % j == 0) {
-					sum++;
-					if (sum == 8) {
-						total++;
-					}
+
+		for (int i = 1; i <= N; i+=2) { // 奇数のみを考慮
+			int divisors = 0;
+			for (int j = 1; j <= i; j++) { // iの約数を探す
+				if (i % j == 0) {
+					divisors++;
 				}
 			}
-			
+			if (divisors == 8) {
+				total++;
+			}
 		}
 		System.out.println(total);			
 	}
