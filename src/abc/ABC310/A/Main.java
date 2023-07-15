8cf948e5a618372a0e38f1938c67ff92caaf84ad
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-07-15 23:53:55 +0900

    participated ABC310AB

---
+++ src/abc/ABC310/A/Main.java
@@ -0,0 +1,33 @@
+// Order Something Else
+
+package abc.ABC310.A;
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	int P = sc.nextInt();
+    	int Q = sc.nextInt();
+    	Integer D[] = new Integer[N];
+    	for (int i = 0; i < N; i++) {
+    		D[i] = sc.nextInt();
+    	}
+    	
+    	int min = D[0];
+    	
+    	for(int j = 1; j < D.length; j++) {
+    	    if(min > D[j]) {
+    	        min = D[j];
+    	    }
+    	}
+    	
+    	if (Q + min < P) {
+    		System.out.println(Q + min);    		
+    	} else {
+    		System.out.println(P);
+    	}
+    	
+	}
+}
