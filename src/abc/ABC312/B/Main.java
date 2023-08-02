44353609f4e7f0c831d8a795cc4290fe4bb92666
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-08-02 23:43:31 +0900

    solved ABC312B

--- src/abc/ABC312/B/Main.java
+++ src/abc/ABC312/B/Main.java
@@ -8,42 +8,53 @@ package abc.ABC312.B;
 
 import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        
-        // 次の行のためにScannerを進める
-        sc.nextLine();
-        
-        char[][] table = new char[N][M];
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		sc.nextLine();
+		char[][] grid = new char[N][M];
+		for (int i = 0; i < N; i++) {
+			String line = sc.nextLine();
+			for (int j = 0; j < M; j++) {
+				grid[i][j] = line.charAt(j);
+			}
+		}
 
-        // 各マスの色を読み取り、白マスだけの行または列があるかどうかをチェックする
-        for (int i = 0; i < N; i++) {
-        	String s = sc.nextLine();
-        	for (int j = 0; j < M; j++) {
-        		table[i][j] = s.charAt(j);
-        		// 左上の TaK Code 判定
-        		if (
-        				// 左上及び右下の縦 3 マス横 3 マスの領域に含まれる計 18 マスは全て黒である
-        				table[i][j] == '#' && table[i][j + 1] == '#' && table[i][j + 2] == '#' &&
-        				table[i + 1][j] == '#' && table[i + 1][j + 1] == '#' && table[i + 1][j + 2] == '#' &&
-        				table[i + 2][j] == '#' && table[i + 2][j + 1] == '#' && table[i + 2][j + 2] == '#' &&
-        				table[i][j] == '#' && table[i][j + 1] == '#' && table[i][j + 2] == '#' &&
-        				table[i + 1][j] == '#' && table[i + 1][j + 1] == '#' && table[i + 1][j + 2] == '#' &&
-        				table[i + 2][j] == '#' && table[i + 2][j + 1] == '#' && table[i + 2][j + 2] == '#' &&
-        				// 左上及び右下の縦 3 マス 横 3 マスの領域に八方位で隣接する計 14 マスは全て白である
-        				table[i][j] == '.' && table[i][j + 1] == '.' && table[i][j + 2] == '.' &&
-        				table[i + 2][j] == '.' && table[i + 2][j + 1] == '.' && table[i + 2][j + 2] == '.' &&
-        				table[i + 2][j] == '.' && table[i + 2][j + 1] == '.' && table[i + 2][j + 2] == '.' &&
-        				table[i + 2][j] == '.' && table[i + 2][j + 1] == '.' && table[i + 2][j + 2] == '.' &&
-        				table[i + 2][j] == '.' && table[i + 2][j + 1] == '.' && table[i + 2][j + 2] == '.' &&
-        				table[i + 2][j] == '.' && table[i + 2][j + 1] == '.' && table[i + 2][j + 2] == '.' &&
-        				table[i + 2][j] == '.' && table[i + 2][j + 1] == '.' && table[i + 2][j + 2] == '.') {
-        			System.out.println(table[i][j] + " " + table[][]);
-        		}
-        	}
-        }
-    }
+		// TaK Codeのパターンを定義
+		char[][] takCode = new char[9][9];
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if ((i < 3 && j < 3) || (i >= 6 && j >= 6)) {
+					takCode[i][j] = '#';
+				} else if ((i < 3 && j >= 6) || (i >= 6 && j < 3)) {
+					takCode[i][j] = '.';
+				} else {
+					takCode[i][j] = '?';
+				}
+			}
+		}
+
+		// グリッド内の全ての9x9領域をチェック
+		for (int i = 0; i <= N - 9; i++) {
+			for (int j = 0; j <= M - 9; j++) {
+				if (isTaKCode(grid, takCode, i, j)) {
+					System.out.println((i + 1) + " " + (j + 1));
+				}
+			}
+		}
+	}
+
+	// 与えられた領域がTaK Codeのパターンに一致するかチェック
+	private static boolean isTaKCode(char[][] grid, char[][] takCode, int x, int y) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (takCode[i][j] != '?' && grid[x + i][y + j] != takCode[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
