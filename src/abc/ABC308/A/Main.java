ab2c3159062469e077a94ceeead2a71cfa44990a
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-08-21 00:23:52 +0900

    solved ABC315A

---
+++ src/abc/ABC308/A/Main.java
@@ -0,0 +1,12 @@
+// New Scheme
+
+package abc.ABC308.A;
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
---
+++ src/abc/ABC315/B/Main.java
@@ -0,0 +1,12 @@
+// The Middle Day
+
+package abc.ABC315.B;
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
