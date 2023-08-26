d8fbb7fe19a5c753df2ac6ab46b15c4eaafeda6f
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-08-27 01:58:47 +0900

    participated ABC317ABC

--- src/abc/ABC317/B/Main.java
+++ src/abc/ABC317/B/Main.java
@@ -1,12 +1,25 @@
-//
+// MissingNo.
 
 package abc.ABC317.B;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int A[] = new int[N];
+		for (int i = 0; i < N; i++) {
+	        A[i] = sc.nextInt();
+		}
+		Arrays.sort(A);
 		
+		for (int i = 0; i < N; i++) {
+			if (A[i] + 1 != A[i+1] && i + 1 < N) {
+				System.out.println(A[i] + 1);
+				break;
+			}
+		}
 	}
 }
