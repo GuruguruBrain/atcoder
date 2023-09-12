dbebfc7bcaffbbb627a5421e65c9ff05201b396e
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-09-12 19:55:41 +0900

    Maintenance

---
+++ Java/tools/Template.java
@@ -0,0 +1,19 @@
+package Java.tools;
+
+public class Template {
+
+}
+
+
+//
+
+package abc.ABC.;
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+	}
+}
