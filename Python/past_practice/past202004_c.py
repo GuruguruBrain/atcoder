033fadfda1f3161102e646556e0644df55b2b2cd
Guruguru_Brain <korosuke.croquette.nari@gmail.com>
2023-09-25 02:13:49 +0900

    solved past202004_c

---
+++ Python/past_practice/past202004_c.py
@@ -0,0 +1,22 @@
+N = int(input())
+
+S = []
+
+for i in range(0, N):
+    si =input()
+    si = list(si) # 受け取った文字列 si を文字のリストに変換
+    S.append(si)
+
+for i in range(N-2, -1, -1): # 下から上へ配列を取り出す
+    for j in range(1, 2*N-2):
+        if S[i][j] == '#':
+            if S[i+1][j-1] == 'X':
+                S[i][j] = 'X'
+            if S[i+1][j] == 'X':
+                S[i][j] = 'X'
+            if S[i+1][j+1] == 'X':
+                S[i][j] = 'X'
+
+for i in range(0, N):
+    S[i] = ''.join(S[i]) # 文字のリストS[i]を結合して、文字列に戻す
+    print(S[i])
